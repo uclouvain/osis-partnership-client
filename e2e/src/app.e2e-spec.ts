2647808bd4820be2b7f96a717f222af73c75f8dc
Alexandra Janin <contact.ajanin@gmail.com>
2019-04-24 12:22:52 +0200

    Add some e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,9 +8,25 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display page title', () => {
     page.navigateTo();
     expect(page.getTitleText()).toEqual('OSIS Partnerships');
+  });
+
+  it('should display Student selected checkbox', () => {
+    expect(page.student.isSelected()).toBe(true);
+  });
+
+  it('should display Staff not selected checkbox', () => {
+    expect(page.staff.isSelected()).toBe(false);
+  });
+
+  it('should display Erasmus checkbox', () => {
+    expect(page.erasmus.isSelected()).toBe(false);
+  });
+
+  it('should display 25 total', () => {
+    expect(page.getTotalCount()).toEqual('25 total');
   });
 
   afterEach(async () => {
