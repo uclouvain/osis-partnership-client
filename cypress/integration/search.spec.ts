c79a43b9e20cd5e8ef735c004f4446caa0a5cd47
Alexandra Janin <contact.ajanin@gmail.com>
2019-04-25 20:06:02 +0200

    Fix test

--- cypress/integration/search.spec.ts
+++ cypress/integration/search.spec.ts
@@ -174,16 +174,13 @@ describe('Search url params', () => {
   });
 
   it('should retrieve url params in form', () => {
-// tslint:disable-next-line: max-line-length
-    cy.visit('http://localhost:4200/#/?ucl_university=e3afa5b4-433d-4eb6-a187-eebb7f759d3b&ucl_university_labo=75799811-6f67-46f7-9143-1e3da672f473&supervisor=74ad955c-88b8-4a95-877d-9340b60cc26a&mobility_type=student');
+    // tslint:disable-next-line: max-line-length
+    cy.visit('http://localhost:4200/#/?continent=Asia&country=JP&city=Tokyo&partner=Middle%20East%20Technical%20University&ucl_university=e3afa5b4-433d-4eb6-a187-eebb7f759d3b&ucl_university_labo=75799811-6f67-46f7-9143-1e3da672f473&supervisor=74ad955c-88b8-4a95-877d-9340b60cc26a&education_field=cf5422b0-8117-42b6-8c81-1d67cd899a27&mobility_type=student');
+    cy.wait('@getConfiguration');
 
     cy.get('[name=ucl_university]').should('have.value', 'FIAL');
     cy.get('[name=ucl_university_labo]').should('have.value', 'ARKE');
     cy.get('[name=supervisor]').should('have.value', 'BRAGARD, Véronique');
-
-// tslint:disable-next-line: max-line-length
-    cy.visit('http://localhost:4200/#/?continent=Asia&country=JP&city=Tokyo&partner=Middle%20East%20Technical%20University&ucl_university=e3afa5b4-433d-4eb6-a187-eebb7f759d3b&ucl_university_labo=75799811-6f67-46f7-9143-1e3da672f473&supervisor=74ad955c-88b8-4a95-877d-9340b60cc26a&education_field=cf5422b0-8117-42b6-8c81-1d67cd899a27&mobility_type=student');
-
     cy.get('[name=continent]').should('have.value', 'Asia');
     cy.get('[name=country]').should('have.value', 'Japon');
     cy.get('[name=city]').should('have.value', 'Tokyo');
