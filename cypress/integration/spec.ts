17fc013f4a728db3e1230358bc41ce1f3ebb2b1d
Alexandra Janin <contact.ajanin@gmail.com>
2019-04-25 12:09:49 +0200

    Delete protractor tests and add cypress tests

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,9 +1,17 @@
-import { navigateTo, getH1 } from '../support/po';
+import { navigateTo, getH1, getInputStudent, getInputStaff } from '../support/po';
 
-describe('Hello Angular', () => {
+describe('OSIS Partnership', () => {
   beforeEach(navigateTo);
 
   it('should display welcome message', () => {
     getH1().contains('OSIS Partnership');
+  });
+
+  it('should display Student selected checkbox', () => {
+    getInputStudent().should('be.checked');
+  });
+
+  it('should display Staff not selected checkbox', () => {
+    getInputStaff().should('not.be.checked');
   });
 });
--- cypress/support/po.ts
+++ cypress/support/po.ts
@@ -3,3 +3,5 @@ const url = 'http://localhost:4200';
 export const navigateTo = () => cy.visit(url);
 
 export const getH1 = () => cy.get('app-root h1');
+export const getInputStudent = () => cy.get('input#Student');
+export const getInputStaff = () => cy.get('input#Staff');
