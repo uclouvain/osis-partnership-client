667f21b5d50ec2520ef60593f94863cea14b8030
Sebastien Corbin <seb.corbin@gmail.com>
2021-02-04 16:11:56 +0100

    Fix regression on forced fields

--- src/app/components/partner-results/partner-results.component.ts
+++ src/app/components/partner-results/partner-results.component.ts
@@ -39,6 +39,7 @@ export class PartnerResultsComponent implements OnInit {
   public visibleMarkers: Partner[] = [];
   public mapInfluence = true;
   public mapHash: string;
+  public initializedWithForcedFields: boolean;
 
   constructor(
     private route: ActivatedRoute,
@@ -48,19 +49,25 @@ export class PartnerResultsComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.initializedWithForcedFields = !!(this.forceUclEntity || this.forcePartnershipType);
     this.route.queryParams.subscribe((queryParams: Params): any => {
-      if (this.forceUclEntity) {
+      if (this.forceUclEntity && this.initializedWithForcedFields) {
         queryParams = {
           ...queryParams,
           ucl_entity: this.forceUclEntity,
         };
       }
-      if (this.forcePartnershipType) {
+      if (this.forcePartnershipType && this.initializedWithForcedFields) {
         queryParams = {
           ...queryParams,
           type: this.forcePartnershipType,
         };
       }
+      // Remove forcing after first request
+      if (this.initializedWithForcedFields) {
+        this.initializedWithForcedFields = false;
+      }
+
       if (queryParams.ordering === undefined) {
         queryParams = { ...queryParams, ordering: 'country_en,city' };
         this.router.navigate([''], {
